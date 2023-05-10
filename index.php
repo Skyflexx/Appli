4245f335d782cf0a88c53e9c049da68f1ced6730
Skyflexx <118121459+Skyflexx@users.noreply.github.com>
2023-05-10 10:07:32 +0200

    add some style

--- index.php
+++ index.php
@@ -1,18 +1,24 @@
-
-
 <!DOCTYPE html>
-
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>Ajout Produit</title>  
 </head>
 
 <body>
+
+<header>
+    <h1>SHOP</h1>
+</header>
     
-    <h1>Ajouter un produit</h1>
+<div class="container">
+   
     <form action ="traitement.php" method="post">
         <p>
             <label>
@@ -33,11 +39,12 @@
             </label>
         </p>
         <p>
-            <input type="submit" name="submit" value="Ajouter le produit">
+            <input class ="btn_submit" type="submit" name="submit" value="Ajouter le produit">
         </p>
     </form>
+</div>
 
     
-    
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>    
 </body>
 </html>
--- recap.php
+++ recap.php
@@ -13,7 +13,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Recapitulatif des produits</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>Récapitulatif</title>  
 </head>
 <body>
 
@@ -28,17 +31,20 @@
 
             // Si une clé existe et qu'il y a des produits qui ont été add, alors on peut afficher ce que l'utilisateur a ajouté :
 
-            echo "<table style='border: 1px solid black'>",
-                    "<thead>", // Thead c'est la ligne entête du tableau.
-                        "<tr>",
-                            "<th>#</th>",
-                            "<th>Nom</th>",
-                            "<th>Prix</th>",
-                            "<th>Quantité</th>",
-                            "<th>Total</th>",
-                        "</tr>",
-                    "</thead>",
-                    "<tbody>";
+            
+
+            echo "<div class='container'>",
+                    "<table class='table table-striped'>",
+                            "<thead>", // Thead c'est la ligne entête du tableau.
+                                "<tr>",
+                                    "<th>#</th>",
+                                    "<th>Nom</th>",
+                                    "<th>Prix</th>",
+                                    "<th>Quantité</th>",
+                                    "<th>Total</th>",
+                                "</tr>",
+                            "</thead>",
+                            "<tbody>";
 
                     $totalGeneral = 0; // initialisation du total qu'on calcule au fur et à mesure du foreach.
 
@@ -66,11 +72,12 @@
             // Fermeture du tableau
                         
             echo "<tr>",
-                    "<td colspan='4' style='border: 1px solid black'>Total Général : </td>",
-                    "<td><strong>".number_format($totalGeneral, 2, ",")."&nbsp;€</strong></td>",
-                    "</tr>",
-                "</tbody>", 
-            "</table>";   
+                        "<td colspan='4' style='border: 1px solid black'>Total Général : </td>",
+                        "<td><strong>".number_format($totalGeneral, 2, ",")."&nbsp;€</strong></td>",
+                        "</tr>",
+                    "</tbody>", 
+                "</table>",
+            "</div>";   
 
             //colspan = '4' permet de fusionner 4 cellules dont le contenu sera "Total général:". Voir resultat car j'ai créé une border pour voir la diff.
 
