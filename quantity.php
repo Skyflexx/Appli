18f84e9007d841449b9d13ff904c90ac134e48f9
Skyflexx <118121459+Skyflexx@users.noreply.github.com>
2023-05-11 10:22:05 +0200

    Add function to add or remove product quantity

--- quantity.php
+++ quantity.php
@@ -4,12 +4,43 @@ session_start(); // démarrage de la session
 
 //////////////
 
+foreach ($_POST as $key => $value) 
+    {
+        if ($value == "-" ){
+
+            foreach ($_SESSION['products'] as $index => $product) 
+            {
+                if (isset($_POST[$index])){
+                    
+                    if ($_SESSION['products'][$index]["qtt"] != 1){
+                    
+                    $_SESSION['products'][$index]["qtt"] -= 1; // Multi dimensionnel array. A chaque strate on selectionne la clé pour accéder à notre variable !
+                    $_SESSION['products'][$index]["total"]= ($_SESSION['products'][$index]["qtt"]) * ($_SESSION['products'][$index]["price"]);   
+                    
+                }                                        
+                    header("Location:recap.php");
+                }
+            }
+
+        } else if($value == "+"){
+
+            foreach ($_SESSION['products'] as $index => $product) 
+            {
+                if (isset($_POST[$index])){        
+
+                    $_SESSION['products'][$index]["qtt"] += 1; // Multi dimensionnel array. A chaque strate on selectionne la clé pour accéder à notre variable !
+                    $_SESSION['products'][$index]["total"]= ($_SESSION['products'][$index]["qtt"]) * ($_SESSION['products'][$index]["price"]);                                                    
+                                        
+                    header("Location:recap.php");        
+                    
+                }
+            }
+
+        }
+
+    }
 
 
-
-
-
-header("Location:recap.php"); // Redirection à la page récap.
 
 
 ?>
