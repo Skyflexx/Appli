40bad8d137d0bd6fb09a65c6f66361ce273a93b3
Skyflexx <118121459+Skyflexx@users.noreply.github.com>
2023-05-12 10:17:22 +0200

    Function simplification

--- quantity.php
+++ quantity.php
@@ -37,17 +37,27 @@ session_start(); // démarrage de la session
 //         }
 //     }
 
+// clic -> puis SHIFT clic => crée une sélection entre les 2 endroits
+// F2 = renommer = renommer un fichier, une balise HTML (avec sa balise fermante)
+// CTRL + F = Find = trouver un élément et surligner toutes les occurrences de cet enchaînement de caractères
+// CTRL + D = placer un curseur supplémentaire à l'endroit de l'occurrence de cet enchaînement de caractères suivante
+// ALT + W = Word-only = CTRL+F et CTRL+D ne prendront en compte que les mots entiers
+// ALT + C = Case-sensitive = CTRL+F et CTRL+D ne prendront en compte que les occurrences correspondants à la casse de la sélection
+// SHIFT + ALT + Flèche H/B = dupliquer une ou plusieurs lignes (la sélection) vers le haut ou le bas
+// CTRL + SHIFT + K = supprimer sélection (lignes entières)
 
 if (isset($_POST['addOne'])){
 
+    
     $_SESSION['products'][$_POST['productIndex']]["qtt"] += 1; // Multi dimensionnel array. A chaque strate on selectionne la clé pour accéder à notre variable !
     $_SESSION['products'][$_POST['productIndex']]["total"]= ($_SESSION['products'][$_POST['productIndex']]["qtt"]) * ($_SESSION['products'][$_POST['productIndex']]["price"]); // Sans oublier de modifier le total du prix en fct de la qtt                                                   
-                                        
+    
     header("Location:recap.php");    
 }
 
 if (isset($_POST['removeOne'])){
 
+    
     if ($_SESSION['products'][$_POST['productIndex']]["qtt"] != 1){ // L'index correspond aux clés du tableau des produits. On s'en sert comme ID unique. Et dans l'affichage du tableau on notera que le nom du bouton prend le nom de l'index.
                     
         $_SESSION['products'][$_POST['productIndex']]["qtt"] -= 1; // Multi dimensionnel array. A chaque strate on selectionne la clé pour accéder à notre variable !
